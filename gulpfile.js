7c99e2ee88556415aeb2dccfe505011f66f693e8
sarah-maris <sarah.87.maris@gmail.com>
2015-11-01 15:55:48 -0500

    Optimize gulp tasks and update bower

--- gulpfile.js
+++ gulpfile.js
@@ -4,43 +4,32 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var del = require('del');
 var imagemin = require('gulp-imagemin');
-var imageminJpegRecompress = require('imagemin-jpeg-recompress');
-var imageResize = require('gulp-image-resize');
-var inlineCss = require('gulp-inline-css');
 var jshint = require('gulp-jshint');
 var uglify = require('gulp-uglify');
 var minifyCSS = require('gulp-minify-css');
 var minifyHTML = require('gulp-minify-html');
+var notify = require('gulp-notify');
 var pngquant = require('imagemin-pngquant');
 var rename = require("gulp-rename");
+var rev = require('gulp-rev');
 var runSequence = require('run-sequence');
 var sourcemaps = require('gulp-sourcemaps');
-var uncss = require('gulp-uncss');
+var usemin = require('gulp-usemin');
+
 
 // Paths to files
 var paths = {
-    styles: ['src/css/**/*.css'],
-    js: ['src/js/*.js'],
+    styles: ['src/css/**/*.css', 'src/fonts/css/*.css'],
+    alljs: [ 'src/components/jquery/dist/jquery.min.js','src/components/knockout/dist/knockout.js', 'src/js/*.js' ],
+    myjs: [ 'src/js/*.js', ],
     html: ['src/*.html'],
     images: ['src/img/*'],
+    fonts: ['src/fonts/font/*']
 }
 
 //Clean out build directory
 gulp.task('clean-build', function (cb) {
   del(['build/**'], cb);
-});
-
-gulp.task('clean', function(cb) {
-    del(['dist/css', 'dist/ajs', 'dist/img'], cb)
-});
-
-
-
-//Compress jpg images
-gulp.task('jpg-images', function () {
-  return gulp.src(paths.images + '.jpg')
-    .pipe(imageminJpegRecompress({loops: 6})())
-    .pipe(gulp.dest('build/img'));
 });
 
 //Compress png images
@@ -54,56 +43,20 @@ gulp.task('png-images', function() {
   .pipe(gulp.dest('build/img'))
 })
 
-//Compress and images
-gulp.task('resize1440', function () {
-  gulp.src(paths.images)
-    .pipe(imageResize({
-      width : 1440,
-      imageMagick: true,
-      crop: false,
-      upscale : false,
-      quality: 0.5
-    }))
-    .pipe(rename(function (path) { path.basename += "_1440px"; }))
-    .pipe(gulp.dest('build/img'));
-});
-
-gulp.task('resize100', function () {
-  gulp.src(paths.images)
-    .pipe(imageResize({
-      width : 100,
-      imageMagick: true,
-      crop: false,
-      upscale : false,
-      quality: 0.3
-    }))
-    .pipe(rename(function (path) { path.basename += "_100px"; }))
-    .pipe(gulp.dest('build/img'));
-});
-
-//Concatenate, remove unused styles and minify css
+//Concatenate and minify css
 gulp.task('min-styles', function(){
   return gulp.src(paths.styles)
     .pipe(sourcemaps.init())
     .pipe(concat('style.css'))
-    .pipe(uncss({
-      html: ['src/*.html']
-      }))
     .pipe(minifyCSS())
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('build/css'));
-});
-
-//Inline css
-gulp.task('inline-css', function() {
-  return gulp.src('build/*.html')
-    .pipe(inlineCss())
-    .pipe(gulp.dest('build/'));
+    .pipe(gulp.dest('build/css'))
+    .pipe( notify('CSS task complete!'));
 });
 
 //Lint js
 gulp.task('lint', function() {
-  return gulp.src(paths.js)
+  return gulp.src(paths.myjs)
     .pipe(jshint())
     .pipe(jshint.reporter('default'))
 });
@@ -120,7 +73,7 @@ gulp.task('min-html', function() {
 
 //Minify js
 gulp.task('min-scripts', function(){
-  return gulp.src(paths.js)
+  return gulp.src(paths.alljs)
     .pipe(sourcemaps.init())
     .pipe(concat('main.js'))
     .pipe(uglify())
@@ -128,46 +81,55 @@ gulp.task('min-scripts', function(){
     .pipe(gulp.dest('build/js/'));
 });
 
+//Transfer fonts to build
+gulp.task('fonts', function() {
+    return gulp.src(paths.fonts)
+     .pipe(gulp.dest('build/font/'));
+});
+
 //Watch for changes, run tasks and notify
 gulp.task('watch', function(){
-  gulp.watch( paths.js, ['min-scripts']);
-  gulp.watch( paths.js, function(event) {
-   console.log('File ' + event.path + ' was ' + event.type + ', running js minification ...');
-  });
-  gulp.watch( paths.styles, ['min-styles']);
-  gulp.watch( paths.styles, function(event) {
-   console.log('File ' + event.path + ' was ' + event.type + ', running css minification ...');
-  });
   gulp.watch( paths.js, ['lint']);
   gulp.watch( paths.js, function(event) {
    console.log('File ' + event.path + ' was ' + event.type + ', linting ...');
   });
-  //gulp.watch( paths.images, ['png-images','jpg-images']);
-  //gulp.watch( paths.images, function(event) {
-  // console.log('File ' + event.path + ' was ' + event.type + ', running image compression ...');
- // });
-  gulp.watch( paths.html, ['min-html']);
-  gulp.watch( paths.html,  function(event) {
-   console.log('File ' + event.path + ' was ' + event.type + ', running html minification ...');
+  gulp.watch( paths.images, ['png-images']);
+  gulp.watch( paths.images, function(event) {
+   console.log('File ' + event.path + ' was ' + event.type + ', running image compression ...');
   });
-
+  gulp.watch( paths.fonts, ['fonts']);
+  gulp.watch( paths.images, function(event) {
+   console.log('File ' + event.path + ' was ' + event.type + ', running font transfer ...');
+  });
 });
 
+gulp.task('usemin', function() {
+  return gulp.src('src/*.html')
+    .pipe(usemin({
+      css: [ minifyCSS(), 'concat', rev() ],
+      html: [ minifyHTML({ empty: true }) ],
+      js: [ uglify(), rev() ],
+      inlinejs: [ uglify() ],
+    //  inlinecss: [ minifyCSS(), 'concat' ]
+    }))
+    .pipe(gulp.dest('build/'));
+});
 
 //Entire project build sequence
 gulp.task('build', function(callback) {
   runSequence(
+
+    // ***REMOVED -- task sequence stops when 'clean-build' is run -- do manually for now ***
     //Clean out build directory
- //   'clean-build', <-- causing hang.  Update later
+    //'clean-build',
+
     //Run synchronous tasks
-    //['min-html',
-	['min-scripts', 'min-styles', 'lint', 'png-images', 'jpg-images'],
-    //Inline css
-   // 'inline-css',
+    ['usemin', 'lint', 'png-images', 'fonts'],
     //Watch for changes
     'watch',
     callback);
 });
-//TODO:  Add image resize when image sizes are determined
-//TODO:  Fix hang in sequence with clean
-//TODO:  Add back inline-css
+//TODO: Add gulp-gh pages
+//TODO: Fix hang in sequence with clean
+//TODO: Add watch path for usemin (html, css)
+//TODO: Add task to create sourcemaps for css and js
