3173e3c12022f47a443cecf816bf505c6408d222
Sarah Maris <sarah@sarahmaris.com>
2018-10-21 23:17:14 -0400

    Use place instead of this in Place constructor

--- src/js/place.js
+++ src/js/place.js
@@ -3,29 +3,28 @@ const Place = function(yelpLocation, category) {
   const place = this;
 
   //Get coordinates for map placement
-  this.lat = yelpLocation.coordinates.latitude;
-  this.lng = yelpLocation.coordinates.longitude;
+  place.lat = yelpLocation.coordinates.latitude;
+  place.lng = yelpLocation.coordinates.longitude;
 
   //Get info for infoWindow
-  this.name = yelpLocation.name;
-  this.id = yelpLocation.id;
+  place.name = yelpLocation.name;
+  place.id = yelpLocation.id;
 
-  this.address = yelpLocation.location.address1;
-  this.rating = yelpLocation.rating;
-  this.stars = ''; //yelpLocation.rating_img_url;
-  this.snippet = ''; //yelpLocation.snippet_text;
-  this.city = yelpLocation.location.city;
-  this.state = yelpLocation.location.state;
-  this.url = yelpLocation.url;
+  place.address = yelpLocation.location.address1;
+  place.rating = yelpLocation.rating || 'no rating available';
+  place.city = yelpLocation.location.city || '';
+  place.state = yelpLocation.location.state || '';
+  place.url = yelpLocation.url || '';
+  place.snippet = '';
 
   //If no image use placeholder
-  this.imgUrl = yelpLocation.image_url
+  place.imgUrl = yelpLocation.image_url
     ? yelpLocation.image_url
     : 'img/no-image.png';
 
   //Format phone number for display
-  this.phone = yelpLocation.display_phone;
   this.dphone = yelpLocation.display_phone;
+  place.phone = yelpLocation.display_phone || '';
 
   //Create a single array of items for search function: categories and business name
   var keywords = [];
@@ -47,15 +46,15 @@ const Place = function(yelpLocation, category) {
   this.fav = false;
 
   //Set marker attributes
-  this.marker = new google.maps.Marker({
-    position: { lat: this.lat, lng: this.lng },
+  place.marker = new google.maps.Marker({
+    position: { lat: place.lat, lng: place.lng },
     map: view.map,
-    title: this.title,
     icon: this.showIcon,
+    title: place.title,
+    id: place.id,
     animation: google.maps.Animation.DROP
   });
 
-  bounds.extend(this.marker.position);
   place.marker.addListener('click', function() {
     place.showYelpDetails();
   });
@@ -73,4 +72,5 @@ const Place = function(yelpLocation, category) {
     }
   };
 
+  bounds.extend(place.marker.position);
 };
